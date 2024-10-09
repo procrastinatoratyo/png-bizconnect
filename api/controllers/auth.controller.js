4e64928ff5700a460d32425ac91e7fb978b8912c
Jamin Tovot <jamintovot@example.com>
2024-10-09 13:57:36 +1000

    create signup api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,17 +1,33 @@
-import User from '../models/user.model.js';
+import User from '../models/user.model.js'; 
+import bcrypt from 'bcryptjs';
 
+export const signup = async (req, res) => { 
 
-export const signup = async (req, res) => 
-    {
-        const{ username, email, password} = req.body;
+    const{ username, email, password} = req.body; 
 
-        if(!username || !email || !password || username=='' || email==''|| password ===''){
-            return res.status(400).json({ message: 'All fields are requires'});
-        }
-        const newUser = new User ({
-            username,
-            email,
-            password,
-        });
+    if(!username || !email || !password || 
+        username=='' ||
+         email==''|| 
+         password ==='') 
+         { 
+        return res.status(400).json({ message: 'All fields are required'}); 
+    } 
 
-    }
+    const hashedPassword = bcrypt.hashSync(password, 10);
+    
+    
+    const newUser = new User
+     ({ 
+        username,
+         email, 
+         password: hashedPassword,
+         });
+
+         try{
+
+        await newUser.save();
+        res.json('Signup successful');
+         }catch (error){
+            res.status(500).json({message: error.message});         }
+
+     };
