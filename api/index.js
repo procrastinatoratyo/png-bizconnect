c6345e92e5f099238abe0ab6852036f2ea14d0ad
Jamin Tovot <jamintovot@example.com>
2024-10-09 11:45:39 +1000

    user models

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import { configDotenv } from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
 import userRoutes from './routes/user.route.js';
-
+import authRoutes from './routes/auth.route.js';
 
 configDotenv();
 
@@ -18,9 +18,12 @@ mongoose.connect(process.env.MONGO)
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
 console.log('Server is running on port 3000');
 
 });
 
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
